1ba42360080c2b0e03b2940602472754afcc5107
Nate Fulkerson <nfulkerson@cps.edu>
2024-11-19 11:21:15 -0600

    Finsihed goal test

--- a5.py
+++ a5.py
@@ -139,7 +139,11 @@ class Board:
         Returns:
             True if we've placed all numbers, False otherwise
         """
-        pass
+        for row in self.rows:
+            for cell in row:
+                if isinstance(cell, list):
+                    return False
+        return True
 
     def update(self, row: int, column: int, assignment: int) -> None:
         """Assigns the given value to the cell given by passed in row and column
